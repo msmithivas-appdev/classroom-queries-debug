80210c9c3964ab43fe00ea9e52c8a18128fd83bb
msmithivas <msmithivas@gmail.com>
2023-03-02 21:23:23 +0000

    Initial commit with partial pages Students and courses so far

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -8,11 +8,11 @@ class DepartmentsController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    @the_id = params.fetch("path_id")
 
-    matching_departments = Department.where({ :id => the_id })
+    @matching_departments = Department.where({ :id => @the_id }).at(0)
 
-    @the_department = matching_departments
+    @the_department = @matching_departments
 
     render({ :template => "departments/show.html.erb" })
   end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,19 @@
 class StudentsController < ApplicationController
   def index
-    matching_students = Student.all
+    @matching_students = Student.all
 
-    @list_of_students = matching_students.order({ :created_at => :desc })
+    @list_of_students = @matching_students.order({ :created_at => :desc })
 
     render({ :template => "students/index.html.erb" })
   end
 
   def show
-    the_id = params.fetch("id")
+    @the_id = params.fetch("path_id")
 
-    matching_students = Enrollment.where({ :id => the_id })
+    @matching_student = Student.where({ :id => @the_id }).at(0)
+    # @matching_students = Enrollment.where({ :student_id => @the_id }).at(0)
 
-    @the_student = matching_students.at(0)
+    @the_student = @matching_student
 
     render({ :template => "students/show.html.erb" })
   end
